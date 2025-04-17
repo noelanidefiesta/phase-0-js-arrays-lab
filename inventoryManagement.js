b543412f54d301832a2d0f99abcfa0f47b81afb1
Your Name <your.email@example.com>
2025-04-17 11:47:56 -0400

    Updates for assignment

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,29 @@
 // Write your code here
+const products = ['Laptop', 'Phone', 'Headphones', 'Monitor']; 
+console.log(products);
+
+function logFirstProduct (name) {
+  products[0]
+};
+
+function addProduct(productName){
+  productName: productName
+  products.push('Mouse');
+}
+console.log(addProduct);
+
+function updateProductName(productName){
+  productName: productName
+}
+products[1] = 'Smartphone'
+console.log(products);
+
+function removeLastProduct(){
+
+};
+products.pop()
+
+console.log(products);
 
 
 
